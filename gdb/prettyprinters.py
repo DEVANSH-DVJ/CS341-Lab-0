7bf2c7a5f11b54a8aa048c82a08a3fa28cb1e941
devansh-dvj <devansh.dvj@gmail.com>
2021-08-04 16:57:01 +0530

    Added more info to pretty print

--- gdb/prettyprinters.py
+++ gdb/prettyprinters.py
@@ -7,14 +7,17 @@ class HeapPrinter:
 
     def to_string(self):
         string = 'VertexHeap object\n'
+        string += 'Capacity: {}\n'.format(self.val['capacity'])
         string += 'Size: {}\n'.format(self.val['size'])
+        string += 'index2HeapIdx: {}\n'.format(self.val['index2HeapIdx'])
+        string += 'heapIdx2index: {}\n'.format(self.val['heapIdx2index'])
         heap = self.val['heap']
         string += 'Heap Address: {}\n'.format(heap)
-        string += 'Heap: '
+        string += 'Heap: {'
         for i in range(self.val['size']):
-            string += '{}: {}, '.format(i, heap.dereference())
+            string += '[{}]: {}, '.format(i, heap.dereference())
             heap += 1
-        return string + '\n'
+        return string[:-2] + '}\n'
 
 
 def custom_printer(val):
