ce838ed324323514c0c13424f1f5d5b9249b16fb
devansh-dvj <devansh.dvj@gmail.com>
2021-08-04 16:43:56 +0530

    Added pretty print for VertexHeap

--- gdb/prettyprinters.py
+++ gdb/prettyprinters.py
@@ -1,9 +1,20 @@
+import gdb
+
+
 class HeapPrinter:
     def __init__(self, val):
         self.val = val
 
     def to_string(self):
-        return 'I am a VertexHeap'
+        string = 'VertexHeap object\n'
+        string += 'Size: {}\n'.format(self.val['size'])
+        heap = self.val['heap']
+        string += 'Heap Address: {}\n'.format(heap)
+        string += 'Heap: '
+        for i in range(self.val['size']):
+            string += '{}: {}, '.format(i, heap.dereference())
+            heap += 1
+        return string + '\n'
 
 
 def custom_printer(val):
