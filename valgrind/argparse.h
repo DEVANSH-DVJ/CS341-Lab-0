c763fe79df821098945564a1339055c6d75ef50f
devansh-dvj <devansh.dvj@gmail.com>
2021-08-05 22:03:40 +0530

    Started valgrind

---
+++ valgrind/argparse.h
@@ -0,0 +1,15 @@
+struct arg {
+  char *name;
+  char *result;
+};
+
+struct argParser {
+  struct arg *argList;
+  int capacity;
+  int len;
+};
+
+void initArgs();
+void addArg(char *name);
+void parseArgs(int argc, char **argv);
+char *getArg();
